cec4c3b0a2aeea54e5c3b56438bee4285e7c8f9f
GogaTsutsunava <91567271+GogaTsutsunava@users.noreply.github.com>
2021-12-18 22:06:52 +0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function myFunction (color){
-    document.getElementById("body").style.background = color;
-}
-function colorChange (color){
-    document.getElementById("body").style.background = color;       
-}
