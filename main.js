3efd5416552e97ddc0962bd84f176a4c402d2447
GogaTsutsunava <91567271+GogaTsutsunava@users.noreply.github.com>
2021-12-18 22:03:48 +0400

    js file

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction (color){
+    document.getElementById("body").style.background = color;
+}
+function colorChange (color){
+    document.getElementById("body").style.background = color;       
+}
